bc364d273b3d8772b720a7bb6cdea3f5e63d709f
yubin07 <qlsdb0725@naver.com>
2022-11-17 03:57:02 +0900

    db7

--- my_settings.py
+++ my_settings.py
@@ -6,8 +6,5 @@ DATABASES = {
         'PASSWORD' : 'rkddbqls12', # Password
         'HOST': 'battery.cq1z58a6la88.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
         'PORT': '3306', # 데이터베이스 포트
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
     }
 }
