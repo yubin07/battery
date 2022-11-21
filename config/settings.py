a8933b2d62858bd2b4155ebf5adb3505486fd47e
yubin07 <qlsdb0725@naver.com>
2022-11-22 00:01:29 +0900

    css1

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    
+    BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
